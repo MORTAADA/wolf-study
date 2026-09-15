7d0cf66865b1ddf38bef1119dc29bb26612ed247
MORTAADA <zamortadha33@gmail.com>
2026-09-15 18:14:23 +0100

    Delete pwa.js

--- pwa.js
+++
@@ -1,7 +0,0 @@
-/* WHITE WOLF V45 — OFFLINE/PWA */
-(function(){
-  if(!("serviceWorker" in navigator)) return;
-  window.addEventListener("load", function(){
-    navigator.serviceWorker.register("./sw.js").catch(function(){});
-  });
-})();
