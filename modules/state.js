0b378f27476cc17cada4c7a0113d970b2a087264
MORTAADA <zamortadha33@gmail.com>
2026-09-16 13:36:19 +0100

    Add files via upload

--- modules/state.js
+++ modules/state.js
@@ -17,5 +17,5 @@
     };
   }
   function createPomodoro(){return {workTime:25,breakTime:5,remaining:25*60,isRunning:false,isBreak:false,timerId:null,freeMode:false};}
-  window.WWState={create:createState,createPomodoro:createPomodoro,version:'63.6'};
+  window.WWState={create:createState,createPomodoro:createPomodoro,version:'63.6.3'};
 })();
